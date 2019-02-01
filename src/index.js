c67cc1f48ea087beb6ec5d976d6db953e00316c1
fr1j0 <fernando.castillo@gig.com>
2019-02-01 18:55:36 +0100

    added styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-class Main{
-    constructor(){
-        console.log('Main')
+import * as PIXI from 'pixi.js'
+import style from './style/style.css'
+
+class Main {
+    Application = PIXI.Application
+    app = new this.Application({
+        width: 240,
+        height: 400,
+        antialias: true,
+        transparent: false,
+        resolution: 1
+    })
+    loader = PIXI.loader
+    resources = PIXI.loader.resources
+    sprite = PIXI.sprite
+
+    constructor() {
+        this.initLayout()
+    }
+
+    initLayout() {
+        document.getElementById('app').appendChild(this.app.renderer.view);
     }
 }
+
+const main = new Main()
